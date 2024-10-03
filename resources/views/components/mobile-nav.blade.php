5081e848c09389e809721ee0c35055723a43459f
iam.raghavan <developer.raghavan@gmail.com>
2024-10-03 10:34:54 +0530

    Refactor views: Remove unnecessary code and improve code structure

--- resources/views/components/mobile-nav.blade.php
+++ resources/views/components/mobile-nav.blade.php
@@ -42,19 +42,20 @@
                 <li class="has-droupdown">
                     <a href="{{ url("#") }}" class="main">About</a>
                     <ul class="submenu mm-collapse">
-                        <li><a class="mobile-menu-link" href="{{ url("/about/our-focus") }}">Our Focus</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Vision & Mission</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">History</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Message From the Chairman</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Message From the Secretary</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Message From the Principal</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Administrative Organization</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Organizational Chart</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Milestones</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">HR Policy</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Audit Statement</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Strategic Plan</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Strategic Plan Meeting Minutes</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/about/our-focus') }}">Our Focus</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/about/vision-and-mission') }}">Vision & Mission</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/about/history') }}">History</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/about/message-from-the-founder-and-chairman') }}">Message From the Chairman</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/about/message-from-the-secretary') }}">Message From the Secretary</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/about/message-from-the-principal') }}">Message From the Principal</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/about/adminstrative-organization') }}">Administrative Organization</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/about/organizational-chart') }}">Organizational Chart</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/about/milestones') }}">Milestones</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/about/hr-policy') }}">HR Policy</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/about/audit-statement') }}">Audit Statement</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/about/strategic-plan') }}">Strategic Plan</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/about/strategic-plan-meeting-minutes') }}">Strategic Plan Meeting Minutes</a></li>
+
 
                     </ul>
                 </li>
@@ -64,49 +65,57 @@
                         <li class="has-droupdown sa">
                             <a href="{{ url("#") }}">Departments</a>
                             <ul class="submenu mm-collapse s">
-                                <li><a class="mobile-menu-link" href="{{ url("#") }}">B.E / B.Tech</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url("#") }}">M.E / M.Tech</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url("#") }}">MCA</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url("#") }}">MBA</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url("#") }}">Science & Humanities</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url("/academics/departments/bachelors") }}">B.E / B.Tech</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url("/academics/departments/masters") }}">M.E / M.Tech</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/academics/departments/postgraduate/master-of-computer-applications') }}">MCA</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/academics/departments/postgraduate/master-of-business-administration') }}">MBA</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/academics/departments/science-humanities') }}">Science & Humanities</a></li>
+
                             </ul>
                         </li>
 
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Industry Academic Partners</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Industry Certification Programmes</a>
-                        </li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Industry Training Programmes</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Advisory Board</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/academics/industry-academic-partnering') }}">Industry Academic Partners</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/academics/industry-certification-programs') }}">Industry Certification Programmes</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/academics/industry-training-programs') }}">Industry Training Programmes</a></li>
+<li><a class="mobile-menu-link" href="{{ url('/academics/advisory-board') }}">Advisory Board</a></li>
+
                     </ul>
                 </li>
 
                 <li class="has-droupdown">
-                    <a href="{{ url("#") }}" class="main">Admissions</a>
+                    <a href="{{ url("#") }}" class="main">Academics</a>
                     <ul class="submenu mm-collapse">
                         <li class="has-droupdown sa">
                             <a href="{{ url("#") }}">Information Brochure</a>
                             <ul class="submenu mm-collapse s">
                                 <li><a class="mobile-menu-link" href="{{ url('#') }}">UG Brochure</a></li>
                                 <li><a class="mobile-menu-link" href="{{ url('#') }}">PG Brochure</a></li>
-                            </ul>
-                        </li>
 
-                        <li><a class="mobile-menu-link" href="{{ url('/admissions/course-intake') }}">Courses & Intake</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url('/admissions/tnea') }}">TNEA {{ date('Y') - 1 }} Information</a></li>
+                            </ul>
+
+
+                        </li>
+   <li><a class="mobile-menu-link" href="{{ url('/admissions/course-intake') }}">Courses & Intake</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/admissions/tnea') }}">TNEA {{ date('Y') }} - {{ date('Y') + 1 }}</a></li>
+
                         <li class="has-droupdown sa">
                             <a href="{{ url("#") }}">Admission Procedure</a>
                             <ul class="submenu mm-collapse s">
-                                <li><a class="mobile-menu-link" href="{{ url('#') }}">UG</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url('#') }}">PG</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url('#') }}">Lateral Entry</a></li>
+                            <li><a class="mobile-menu-link" href="{{ url('/admission/admission-procedure/undergraduate') }}">UG</a></li>
+                            <li><a class="mobile-menu-link" href="{{ url('/admission/admission-procedure/postgraduate') }}">PG</a></li>
+                            <li><a class="mobile-menu-link" href="{{ url('/admission/admission-procedure/lateral-entry') }}">Lateral Entry</a></li>
                             </ul>
                         </li>
                         <li><a class="mobile-menu-link" href="{{ url('/admissions/excess-fee-committee-circular') }}">Excess Fee Committee Circular</a></li>
                         <li><a class="mobile-menu-link" href="{{ url('/admissions/complaint-committee') }}">Complaint Committee</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url('#') }}">Why Study Here</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url('#') }}">FAQ</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/admissions/why-study-here') }}">Why Study Here</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/admissions/faq') }}">FAQ</a></li>
+
                     </ul>
                 </li>
+
+
+
 
 
                 <li class="has-droupdown">
@@ -154,12 +163,12 @@
                         <li class="has-droupdown sa">
                             <a href="{{ url('#') }}">Professional Bodies</a>
                             <ul class="submenu mm-collapse s">
-                                <li><a class="mobile-menu-link" href="{{ url('#') }}">IEEE</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url('#') }}">ISTE</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url('#') }}">ICTACT</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url('#') }}">CSI</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url('#') }}">IETE</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url('#') }}">IEI</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/activities/professional-bodies/ieee') }}">IEEE</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/activities/professional-bodies/iste') }}">ISTE</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/activities/professional-bodies/ictact') }}">ICTACT</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/activities/professional-bodies/csi') }}">CSI</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/activities/professional-bodies/iete') }}">IETE</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/activities/professional-bodies/iei') }}">IEI</a></li>
                             </ul>
                         </li>
                         <li><a class="mobile-menu-link" href="{{ url('/activities/anti-ragging-committee') }}">Anti Ragging Committee</a></li>
@@ -180,49 +189,70 @@
                 <li class="has-droupdown">
                     <a href="{{ url("#") }}" class="main">Placements</a>
                     <ul class="submenu mm-collapse">
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Industry Partnership & Corporate Affairs
-                                (Placement & Training)</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">MoU</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Recruiters</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Training Cell</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Placement Team</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Placement Statistics</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Our Few Of Recruiters</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/placements#industry-academic-partnering') }}">Industry Partnership & Corporate Affairs (Placement & Training)</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/placements#memorandum-of-understanding') }}">MoU</a></li>
+                        {{-- <li><a class="mobile-menu-link" href="{{ url('#') }}">Recruiters</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('#') }}">Training Cell</a></li> --}}
+                        <li><a class="mobile-menu-link" href="{{ url('/placements#placementTeam') }}">Placement Team</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/placements#statistics') }}">Placement Statistics</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/placements#our-top-recruiters') }}">Our Few Of Recruiters</a></li>
                     </ul>
                 </li>
+
                 <li class="has-droupdown">
                     <a href="{{ url("#") }}" class="main">Research</a>
                     <ul class="submenu mm-collapse">
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Team Members</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Research Supervisor and Scholar</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Publications</a></li>
+
+                        <li><a class="mobile-menu-link" href="{{ url('/research/research-and-development') }}">Team Members</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/research/research-supervisor-and-scholar-details') }}">Research Supervisor and Scholar</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/research/publication-details') }}">Publications</a></li>
+
+                        <li><a class="mobile-menu-link" href="{{ url('/research/patent-details') }}">Industry Collaboration</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/research/copyrights') }}">Copyrights</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url('/research/r-and-d-policy') }}">R &amp; D Policy</a></li>
 
                         <li class="has-droupdown sa">
                             <a href="{{ url("#") }}">Projects</a>
                             <ul class="submenu mm-collapse s">
-                                <li><a class="mobile-menu-link" href="{{ url("#") }}">MECH</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url("#") }}">CIVIL</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url("#") }}">EEE</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url("#") }}">ECE</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url("#") }}">CSE</a></li>
-                                <li><a class="mobile-menu-link" href="{{ url("#") }}">IT</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/research/projects/mechanical-engineering') }}">MECH</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/research/projects/civil-engineering') }}">CIVIL</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/resources/projects/electrical-and-electronics-engineering') }}">EEE</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/research/projects/electronics-and-communication-engineering') }}">ECE</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/resources/projects/computer-science-and-engineering') }}">CSE</a></li>
+                                <li><a class="mobile-menu-link" href="{{ url('/resources/projects/information-technology') }}">IT</a></li>
+
                             </ul>
                         </li>
 
 
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Industry Collabration</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Copy Rights</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">R & D Policy</a></li>
+
+                    </ul>
+                </li>
+
+
+
+                <li class="has-droupdown">
+                    <a href="{{ url("#") }}" class="main">Alumni</a>
+                    <ul class="submenu mm-collapse">
+                        <li><a class="mobile-menu-link" href="{{ url("") }}">Alumni Interaction</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url("") }}">Alumni Survey</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url("") }}">Alumni Magazine</a></li>
+
                     </ul>
                 </li>
                 <li class="has-droupdown">
-                    <a href="{{ url("#") }}" class="main">Circular</a>
+                    <a class="main" href="{{ url("#") }}">Downloads</a>
+                </li>
+
+                <li class="has-droupdown">
+                    <a href="{{ url("#") }}" class="main">IQAC</a>
                     <ul class="submenu mm-collapse">
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Anna University Circular</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">UGC Guidelines</a></li>
-                        <li><a class="mobile-menu-link" href="{{ url("#") }}">Fees Reminder for the Academic Year {{ date('Y') }} - {{ substr(date('Y') + 1, -2) }}</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url("/iqac/committe-members") }}">Members</a></li>
+                        <li><a class="mobile-menu-link" href="{{ url("/iqac/meeting-minutes") }}">IQAC Meeting Minutes</a></li>
+
                     </ul>
                 </li>
+
             </ul>
         </nav>
 
@@ -233,10 +263,10 @@
                 <div class="offcanvase__info--content--social">
                     <p class="title">Follow Us:</p>
                     <div class="social__links">
-                        <a href="{{ url("#") }}"><i class="fa-brands fa-facebook"></i></a>
-                        <a href="{{ url("#") }}"><i class="fa-brands fa-instagram"></i></a>
-                        <a href="{{ url("#") }}"><i class="fa-brands fa-linkedin"></i></a>
-                        <a href="{{ url("#") }}"><i class="fa-brands fa-youtube"></i></a>
+                        <a href="https://www.facebook.com/EGSPGOI/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                        <a href="https://www.instagram.com/egspillay_group_of_institution/"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="https://www.linkedin.com/company/egspgoi/"><i class="fa-brands fa-linkedin"></i></a>
+                        <a href="https://www.youtube.com/@E.G.S.PillayGroupofInstitution"><i class="fa-brands fa-youtube"></i></a>
                     </div>
                 </div>
             </div>
