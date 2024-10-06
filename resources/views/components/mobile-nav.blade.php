e0eef830a18448c11f1bb1adffcaead19bc591e1
iam.raghavan <developer.raghavan@gmail.com>
2024-10-06 10:50:36 +0530

    Refactor header and mobile navigation components to update Downloads link

--- resources/views/components/mobile-nav.blade.php
+++ resources/views/components/mobile-nav.blade.php
@@ -242,7 +242,8 @@
                     </ul>
                 </li>
                 <li class="has-droupdown">
-                    <a class="main" href="{{ url("#") }}">Downloads</a>
+                    <a class="main" href="https://coe.egspec.org/" target="_blank" referrerpolicy="origin-when-cross-origin" rel="noopener noreferrer">Downloads</a>
+                    {{-- <a class="main" href="{{ url("#") }}">Downloads</a> --}}
                 </li>
 
                 <li class="has-droupdown">
