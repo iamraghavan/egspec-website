b34d74b60a2d5ca79299a46c709ccf38e2d3ac97
egspec <web@egspec.org>
2024-10-09 10:25:25 +0000

    update it

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -24,7 +24,7 @@
                     <div class="header-top-one">
                         <div class="left-information">
                             <a href="mailto:enquires@egspec.org" class="email"><i class="fa-light fa-envelope"></i>enquires@egspec.org</a>
-                            <a href="tel:04365251114" class="email"><i class="fa-light fa-phone"></i>04365 251 114</a>
+                            <a href="tel:7373 067 032" class="email"><i class="fa-light fa-phone"></i>7373 067 032 | 8680 954 537</a>
                         </div>
                         <!-- right area -->
                         <div class="right-information">
