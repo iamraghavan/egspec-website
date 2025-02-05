450ec37a11f49eec288279bb19b838b4a4425501
bumblebeesindia <raghavanofficials@gmail.com>
2025-02-05 10:57:03 +0530

    Add admission form component to application area

---
+++ resources/views/components/admission-form.blade.php
@@ -0,0 +1,80 @@
+<section class="rts-application-area moving pt--120 rts-application-area-bg">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="rts-section rt-center mb--50 w-460">
+                <h2 class="rts-section-title mb--10 rt-white">Apply for Admission</h2>
+                <p class="rt-white">Welcome to the gateway of possibilities your admission to Studyhub University. At Studyhub, we understand.</p>
+            </div>
+        </div>
+        <!-- application form -->
+        <div class="row justify-content-md-center">
+            <div class="col-md-11 col-lg-6 col-xl-7">
+                <div class="rts-admission-form-image">
+                    <img src="assets/images/feature/admission-form.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-md-11 col-lg-6 col-xl-5">
+                <div class="rts-application-form">
+                    <div class="rts-application-form-content">
+                        <h5 class="heading-title  mb--25">Application Form</h5>
+                        <form action="#" class="rts-form">
+                            <div class="rts-input rt-flex">
+                                <div class="single-input">
+                                    <input type="text" name="fname" id="fname" placeholder="First Name" required="" fdprocessedid="5r6yq">
+                                </div>
+                                <div class="single-input">
+                                    <input type="text" name="lname" id="lname" placeholder="Last Name" required="" fdprocessedid="vv9at">
+                                </div>
+                            </div>
+                            <div class="rts-input rt-flex">
+                                <div class="single-input">
+                                    <input type="email" name="email" id="email2" placeholder="Email.." required="" fdprocessedid="8vlemn">
+                                </div>
+                                <div class="single-input">
+                                    <input type="number" name="Phone" id="Phone" placeholder="Phone" required="" fdprocessedid="ssv797">
+                                </div>
+                            </div>
+
+                            <div class="rts-input rt-flex">
+                                <div class="single-input">
+                                    <input type="text" name="address" id="address" placeholder="address" required="" fdprocessedid="uxg4e4">
+                                </div>
+                                <div class="single-input">
+                                    <input type="text" name="city" id="city" placeholder="City" required="" fdprocessedid="wdd4us">
+                                </div>
+                            </div>
+
+                            <div class="rts-input rt-flex">
+                                <div class="single-input">
+                                    <input type="text" name="country" id="country" placeholder="Country" required="" fdprocessedid="1ted6">
+                                </div>
+                                <div class="single-input">
+                                    <input type="number" name="zip" id="zip" placeholder="Zip Code" required="" fdprocessedid="p5a2l">
+                                </div>
+                            </div>
+                            <div class="rts-input w-full">
+                                <div class="single-input">
+                                    <input type="text" name="data" id="datepicker" placeholder="dd/mm/yy" required="" class="hasDatepicker" fdprocessedid="09g5ib">
+                                </div>
+                            </div>
+                            <div class="rts-input w-full">
+                                <div class="single-input">
+                                    <textarea name="msg" id="msg" placeholder="Enter Your Comments"></textarea>
+                                </div>
+                            </div>
+                            <button type="submit" class="rts-theme-btn rts-nbg-btn btn-arrow v2 full-btn" fdprocessedid="m6pqcw">Apply Now <span><i class="fa-sharp fa-regular fa-arrow-right"></i></span></button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="rt-shape">
+            <img src="assets/images/feature/shape/01.svg" data-speed="0.04" alt="shape" class="rt-shape__1">
+            <img src="assets/images/feature/shape/02.svg" data-speed="0.04" alt="shape" class="shape rt-shape__2" style="transform: matrix(1, 0, 0, 1, -20.3, -9.18);">
+            <img src="assets/images/feature/shape/03.svg" data-speed="0.04" alt="shape" class="shape rt-shape__3" style="transform: matrix(1, 0, 0, 1, -20.3, -9.18);">
+            <img src="assets/images/feature/shape/04.svg" data-speed="0.04" alt="shape" class="shape rt-shape__4" style="transform: matrix(1, 0, 0, 1, -20.3, -9.18);">
+            <img src="assets/images/feature/shape/05.svg" data-speed="0.04" alt="shape" class="shape rt-shape__5" style="transform: matrix(1, 0, 0, 1, -20.3, -9.18);">
+        </div>
+    </div>
+</section>
