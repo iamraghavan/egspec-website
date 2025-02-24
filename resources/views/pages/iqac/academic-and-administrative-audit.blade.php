61021c0c78a19958855359b9dc3c698245e66159
bumblebeesindia <raghavanofficials@gmail.com>
2025-02-24 12:19:53 +0530

    Enhance PDF loading functionality with a loader and update educational objectives in Biomedical Engineering program

--- resources/views/pages/iqac/academic-and-administrative-audit.blade.php
+++ resources/views/pages/iqac/academic-and-administrative-audit.blade.php
@@ -4,69 +4,83 @@
 'value_1' => 'IQAC',
 'value_2' => '',
 'value_3' => '',
-'page_title' => 'ACADEMIC AND ADMINISTRATIVE AUDIT (AAA)'
+'page_title' => 'ACADEMIC AND ADMINISTRATIVE AUDIT'
 ])
-
-
 
 <div class="rts-page-content rts-section-padding">
     <div class="container">
         <div class="row">
             <div class="col-12 my-5">
                 <div class="program-description-area">
-
                     <div class="program-credit-area">
-                        <h3 class="rts-section-title animated fadeIn">ACADEMIC AND ADMINISTRATIVE AUDIT (AAA)</h3>
-
+                        <h3 class="rts-section-title animated fadeIn">ACADEMIC AND ADMINISTRATIVE AUDIT</h3>
                         <div class="program-accordion my-5">
                             <div class="accordion" id="rts-accordion">
-
                                 <div class="accordion-item">
                                     <div>
                                         <div class="accordion-body-content">
                                             <table class="table">
                                                 <tbody>
                                                     <tr>
-                                                        <td><a href="javascript:void(0);" onclick="loadPDF('https://firebasestorage.googleapis.com/v0/b/egspj-bumblebees.appspot.com/o/egspec-assets%2Fpdf%2Facademic-and-administrative-audit-aaa.pdf?alt=media&token=e2d1bd79-48bc-4923-8e7a-d652ccdc8b0e')">ACADEMIC AND ADMINISTRATIVE AUDIT (AAA)</a></td>
+                                                        <td><a href="javascript:void(0);" onclick="loadPDF('https://firebasestorage.googleapis.com/v0/b/egspj-bumblebees.appspot.com/o/egspec-assets%2Fpdf%2Facademic-and-administrative-audit-aaa.pdf?alt=media&token=e2d1bd79-48bc-4923-8e7a-d652ccdc8b0e')">ACADEMIC AND ADMINISTRATIVE AUDIT TEMPLATE</a></td>
                                                         <td><i class="fa fa-file-pdf"></i> / PDF File</td>
                                                     </tr>
                                                     <tr>
                                                         <td><a href="javascript:void(0);" data-hystmodal="#auditReportsModal">Audit reports</a></td>
                                                         <td><i class="fa fa-external-link"></i> / Open Link</td>
                                                     </tr>
-
-
                                                 </tbody>
                                             </table>
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
-
                 </div>
 
-
+                <div id="loader">Loading...</div>
                 <div class="iframe-div">
                     <iframe id="pdfIframe" src="" frameborder="0" scrolling="auto" height="100%" width="100%" style="height: 100rem !important;"></iframe>
                 </div>
 
-                <script>
-                    // Function to load the PDF into the iframe
-                    function loadPDF(pdfUrl) {
-    var iframe = document.getElementById('pdfIframe');
-    // Format the URL for Google Docs Viewer
-    iframe.src = 'https://drive.google.com/viewerng/viewer?embedded=true&url=' + encodeURIComponent(pdfUrl);
-}
-
-                </script>
             </div>
-
         </div>
     </div>
 </div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to load the PDF into the iframe
+    function loadPDF(pdfUrl) {
+        var iframe = document.getElementById('pdfIframe');
+        var loader = document.getElementById('loader');
+
+        // Show loader
+        loader.style.display = 'block';
+
+        // Hide iframe initially
+        iframe.style.display = 'none';
+
+        // Format the URL for Google Docs Viewer
+        iframe.src = 'https://drive.google.com/viewerng/viewer?embedded=true&url=' + encodeURIComponent(pdfUrl);
+
+        // When the iframe loads, hide the loader and show the iframe
+        iframe.onload = function() {
+            loader.style.display = 'none';
+            iframe.style.display = 'block';
+        };
+    }
+
+    // Assign the loadPDF function to the onclick event
+    document.querySelectorAll('a[href="javascript:void(0);"]').forEach(function(link) {
+        link.onclick = function() {
+            var pdfUrl = this.getAttribute('onclick').match(/loadPDF\('([^']+)'\)/)[1];
+            loadPDF(pdfUrl);
+        };
+    });
+});
+</script>
 
 <style>
     .iframe-div {
